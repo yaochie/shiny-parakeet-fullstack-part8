9c85eef9647b8df1b88a1dab4efe0c1282c11f58
yc <10258156+yaochie@users.noreply.github.com>
2020-05-01 17:39:42 -0400

    exercise 8.10

--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,6 +10,13 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ addBook ] = useMutation(ADD_BOOK, {
+    refetchQueries: [
+      { query: ALL_BOOKS },
+      { query: ALL_AUTHORS }
+    ]
+  })
+
   if (!props.show) {
     return null
   }
@@ -14,7 +24,14 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
     
-    console.log('add book...')
+    addBook({
+      variables: {
+        title,
+        published: Number(published),
+        author,
+        genres
+      }
+    })
 
     setTitle('')
     setPublished('')
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -20,3 +20,18 @@ query {
 }
 `
 
+export const ADD_BOOK = gql`
+mutation addBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+  addBook(
+    title: $title,
+    published: $published,
+    author: $author,
+    genres: $genres
+  ) {
+    title
+    published
+    genres
+  }
+}
+`
+
